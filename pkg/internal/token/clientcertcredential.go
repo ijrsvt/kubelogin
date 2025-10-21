672fb082d67de13a4fc21a550976151911fd67a5
Ian Rodney <ian.rodney@gmail.com>
2025-10-21 15:26:03 -0700

    use the new go-pkcs12 module

--- pkg/internal/token/clientcertcredential.go
+++ pkg/internal/token/clientcertcredential.go
@@ -13,7 +13,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity/cache"
-	"golang.org/x/crypto/pkcs12"
+	"software.sslmate.com/src/go-pkcs12"
 	"k8s.io/klog/v2"
 )
 
@@ -87,103 +87,27 @@ func (c *ClientCertificateCredential) NeedAuthenticate() bool {
 	return false
 }
 
-func isPublicKeyEqual(key1, key2 *rsa.PublicKey) bool {
-	if key1.N == nil || key2.N == nil {
-		return false
-	}
-	return key1.E == key2.E && key1.N.Cmp(key2.N) == 0
-}
-
-func splitPEMBlock(pemBlock []byte) (certPEM []byte, keyPEM []byte) {
-	for {
-		var derBlock *pem.Block
-		derBlock, pemBlock = pem.Decode(pemBlock)
-		if derBlock == nil {
-			break
-		}
-		switch derBlock.Type {
-		case "CERTIFICATE":
-			certPEM = append(certPEM, pem.EncodeToMemory(derBlock)...)
-		case "PRIVATE KEY":
-			keyPEM = append(keyPEM, pem.EncodeToMemory(derBlock)...)
-		}
-	}
-
-	return certPEM, keyPEM
-}
-
-func parseRsaPrivateKey(privateKeyPEM []byte) (*rsa.PrivateKey, error) {
-	block, _ := pem.Decode(privateKeyPEM)
-	if block == nil {
-		return nil, fmt.Errorf("failed to decode a pem block from private key")
-	}
-
-	privatePkcs1Key, errPkcs1 := x509.ParsePKCS1PrivateKey(block.Bytes)
-	if errPkcs1 == nil {
-		return privatePkcs1Key, nil
-	}
-
-	privatePkcs8Key, errPkcs8 := x509.ParsePKCS8PrivateKey(block.Bytes)
-	if errPkcs8 == nil {
-		privatePkcs8RsaKey, ok := privatePkcs8Key.(*rsa.PrivateKey)
-		if !ok {
-			return nil, fmt.Errorf("pkcs8 contained non-RSA key. Expected RSA key")
-		}
-		return privatePkcs8RsaKey, nil
-	}
-
-	return nil, fmt.Errorf("failed to parse private key as Pkcs#1 or Pkcs#8. (%w), (%w)", errPkcs1, errPkcs8)
-}
-
-func parseKeyPairFromPEMBlock(pemBlock []byte) (*x509.Certificate, *rsa.PrivateKey, error) {
-	certPEM, keyPEM := splitPEMBlock(pemBlock)
-
-	privateKey, err := parseRsaPrivateKey(keyPEM)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	found := false
-	var cert *x509.Certificate
-	for {
-		var certBlock *pem.Block
-		var err error
-		certBlock, certPEM = pem.Decode(certPEM)
-		if certBlock == nil {
-			break
-		}
-
-		cert, err = x509.ParseCertificate(certBlock.Bytes)
-		if err != nil {
-			return nil, nil, fmt.Errorf("unable to parse certificate: %w", err)
-		}
-
-		certPublicKey, ok := cert.PublicKey.(*rsa.PublicKey)
-		if ok && isPublicKeyEqual(certPublicKey, &privateKey.PublicKey) {
-			found = true
-			break
-		}
-	}
-
-	if !found {
-		return nil, nil, fmt.Errorf("unable to find a matching public certificate")
-	}
-
-	return cert, privateKey, nil
-}
-
 func decodePkcs12(pkcs []byte, password string) (*x509.Certificate, *rsa.PrivateKey, error) {
-	blocks, err := pkcs12.ToPEM(pkcs, password)
+	privateKey, cert, certChain, err := pkcs12.DecodeChain(pkcs, password)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	var pemData []byte
-	for _, b := range blocks {
-		pemData = append(pemData, pem.EncodeToMemory(b)...)
+	rsaPrivateKey, ok := privateKey.(*rsa.PrivateKey)
+	if !ok {
+		return nil, nil, fmt.Errorf("expected rsa private key, got: %T", privateKey)
 	}
 
-	return parseKeyPairFromPEMBlock(pemData)
+	// DecodeChain assumes the fist certificate is the one corresponding to the publicKey.
+	// Maintain backwards compatability by checking all certs for the one that matches
+	// the RSA Private Key.
+	for _, c := range append([]*x509.Certificate{cert}, certChain...) {
+		if rsaPrivateKey.PublicKey.Equal(c.PublicKey) {
+			return c, rsaPrivateKey, nil
+		}
+	}
+
+	return nil, nil, fmt.Errorf("unable to find a matching public certificate")
 }
 
 func readCertificate(certFile, password string) (*x509.Certificate, *rsa.PrivateKey, error) {
